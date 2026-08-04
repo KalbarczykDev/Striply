e4deb382d32d7244827d2d01ebdb96119abfe638
Oskar Kalbarczyk <oskalbarczyk@gmail.com>
2026-08-04 20:40:02 +0200

    Initial commit

---
+++ src/main/java/dev/kalbarczyk/striply/StriplyApplication.java
@@ -0,0 +1,13 @@
+package dev.kalbarczyk.striply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StriplyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StriplyApplication.class, args);
+    }
+
+}
